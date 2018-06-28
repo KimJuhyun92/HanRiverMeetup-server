deaa003b8889756bbc5fe8b3c83df0527ce5f4b1
leemiyoung <leemy0102@naver.com>
2018-06-28 21:19:18 +0900

    #7 application create, delete

--- src/main/java/com/hangang/HangangRiver/HangangRiverApplication.java
+++ src/main/java/com/hangang/HangangRiver/HangangRiverApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HangangRiverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HangangRiverApplication.class, args);
 	}
