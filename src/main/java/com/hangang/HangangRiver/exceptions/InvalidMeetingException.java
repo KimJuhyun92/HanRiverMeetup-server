d1a4ab85dbec55e64bb2831c1be31bd85d27eb8d
Unknown <kimts2002kr@naver.com>
2018-07-10 09:59:30 +0900

    #16 예외 메시지 오타 수정

--- src/main/java/com/hangang/HangangRiver/exceptions/InvalidMeetingException.java
+++ src/main/java/com/hangang/HangangRiver/exceptions/InvalidMeetingException.java
@@ -3,6 +3,6 @@ package com.hangang.HangangRiver.exceptions;
 public class InvalidMeetingException extends Exception {
     @Override
     public String getMessage() {
-        return "존재하지 않는 모입입니다.";
+        return "존재하지 않는 모임입니다.";
     }
 }
