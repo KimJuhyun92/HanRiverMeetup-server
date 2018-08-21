3c4ac5951b5980acfb3473923dce0b4e6435bf8e
Unknown <kimts2002kr@naver.com>
2018-08-21 21:29:45 +0900

    #70 카테고리 목록을 불러오는 API 작성
    * 카테고리 목록을 불러오는 Web Service 제작
    * 모델 작성

---
+++ src/main/java/com/hangang/HangangRiver/meeting/dao/ActivityMapper.java
@@ -0,0 +1,9 @@
+package com.hangang.HangangRiver.meeting.dao;
+
+import com.hangang.HangangRiver.meeting.model.Activity;
+
+import java.util.List;
+
+public interface ActivityMapper {
+    List<Activity> getAllActivities();
+}
