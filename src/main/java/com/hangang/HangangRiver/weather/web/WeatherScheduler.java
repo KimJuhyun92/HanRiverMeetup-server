7dd719517849db30060e745986fc71fe7e665dc6
LeeMiYoung <leemy0102@naver.com>
2018-08-24 17:55:06 +0900

    #69 날씨 조회 API 수정 테스트필요.

--- src/main/java/com/hangang/HangangRiver/weather/web/WeatherScheduler.java
+++ src/main/java/com/hangang/HangangRiver/weather/web/WeatherScheduler.java
@@ -1,14 +1,12 @@
 package com.hangang.HangangRiver.weather.web;
 
-import static org.hamcrest.CoreMatchers.containsString;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 import org.json.simple.JSONArray;
@@ -28,23 +26,80 @@ public class WeatherScheduler {
 	private String todayTmx = "0";
 	private String nx = "59";
 	private String ny = "126";
+	private String serviceKey = "wHkULcMomTpz3wVCYqSbrap5VrtdRQrZSTLd%2BcX4pwaZTjaVpDj9cF5fydh7%2BapE1tAwOBFUMJPrsTOBWTJUfA%3D%3D";
 
 	@Autowired
 	WeatherService weatherService;
 
-	public Weather getNowWeather() throws ParseException, IOException{
+	@Scheduled(cron="0 45 * * * *")//매시간 45분
+	public void setWeatherInformation() throws IOException, ParseException{
+		Weather weatherInformation = new Weather();
+		weatherInformation = getNowWeather();
+		if (Double.parseDouble(weatherInformation.getT1h()) < Double.parseDouble(todayTmn)) {//최저온도가 현재온도보다 높을경우
+			weatherInformation.setTmn(weatherInformation.getT1h());
+			weatherInformation.setTmx(todayTmx);
+		} else if (Double.parseDouble(weatherInformation.getT1h()) > Double.parseDouble(todayTmx)) {//최고온도가 현재온도보다 낮을경우
+			weatherInformation.setTmn(todayTmn);
+			weatherInformation.setTmx(weatherInformation.getT1h());
+		} else {
+			weatherInformation.setTmn(todayTmn);
+			weatherInformation.setTmx(todayTmx);
+		}
+		weatherService.createWeatherInformation(weatherInformation);
+	}
+
+	public Weather getNowWeather() throws IOException, ParseException{
 		//현재기온, 현재 구름 상태 , 현재 강수상태 조회
 		Date today = new Date();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 		SimpleDateFormat timeFormat = new SimpleDateFormat("HH");
+		String baseTime = timeFormat.format(today)+"40";
+		String service = "ForecastGrib";
+		String serviceValue = "obsrValue";
+		Weather resultWeather = callWeatherAPI(baseTime, service, serviceValue);
+		return resultWeather;
+	}
+
+	@Scheduled(cron=" 0 10 02 * * * ")//새벽 2시 10분에 2시데이터 한번 조회
+	public void setMinMaxWeather() throws IOException, ParseException{
+		// 오늘 최저,최고기온 조회
+		String baseTime = "0200";
+		String service = "ForecastSpaceData";
+		String serviceValue = "fcstValue";
+		Weather resultWeather = callWeatherAPI(baseTime, service, serviceValue);
+	}
+
+	@Scheduled(cron=" 0 10 05 * * * ")//새벽 5시 10분에 5시데이터 한번 조회
+	public void setMaxWeatherStep2() throws IOException, ParseException{
+		String baseTime = "0500";
+		String service = "ForecastSpaceData";
+		String serviceValue = "fcstValue";
+		Weather resultWeather = callWeatherAPI(baseTime, service, serviceValue);
+	}
+
+	@Scheduled(cron="0 10 08 * * *")//새벽 8시 10분에 8시데이터 한번 조회
+	public void setMaxWeatherStep3() throws IOException, ParseException{
+		String baseTime = "0800";
+		String service = "ForecastSpaceData";
+		String serviceValue = "fcstValue";
+		Weather resultWeather = callWeatherAPI(baseTime, service, serviceValue);
+	}
+
+	@Scheduled(cron="0 10 11 * * *")//오전 11시 10분에 11시데이터 한번 조회
+	public void setMaxWeatherStep4() throws IOException, ParseException{
+		String baseTime = "1100";
+		String service = "ForecastSpaceData";
+		String serviceValue = "fcstValue";
+		Weather resultWeather = callWeatherAPI(baseTime, service, serviceValue);
+	}
+
+	public Weather callWeatherAPI(String baseTime, String service, String serviceValue) throws IOException, ParseException {
+		Date today = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 		String baseDate = dateFormat.format(today);
-		//String baseTime = timeFormat.format(today)+"40";
-		String baseTime = "2240";
-		String serviceKey = "wHkULcMomTpz3wVCYqSbrap5VrtdRQrZSTLd%2BcX4pwaZTjaVpDj9cF5fydh7%2BapE1tAwOBFUMJPrsTOBWTJUfA%3D%3D";
-		String service = "ForecastGrib"; //ForecastSpaceData or ForecastGrib
 		String urlStr = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/"+service+"?"
-	            + "serviceKey=" + serviceKey + "&base_date=" + baseDate + "&base_time=" + baseTime
-	            + "&nx="+ nx + "&ny=" + ny + "&_type=json";
+						+ "serviceKey=" + serviceKey + "&base_date=" + baseDate + "&base_time=" + baseTime
+						+ "&nx="+ nx + "&ny=" + ny + "&_type=json";
+
 		URL url = new URL(urlStr);
 		BufferedReader bf;
 		String line = "";
@@ -52,53 +107,45 @@ public class WeatherScheduler {
 
 		bf = new BufferedReader(new InputStreamReader(url.openStream()));
 
-        while((line=bf.readLine())!=null){
-            result=result.concat(line);
-        }
-        JSONParser parser = new JSONParser();
-        JSONObject obj = (JSONObject) parser.parse(result);
+		while((line=bf.readLine())!=null){
+			result=result.concat(line);
+		}
+		JSONParser parser = new JSONParser();
+		JSONObject obj = (JSONObject) parser.parse(result);
 
-        JSONObject parse_response = (JSONObject) obj.get("response");
-        JSONObject parse_body = (JSONObject) parse_response.get("body");
-        JSONObject parse_items = (JSONObject) parse_body.get("items");
-        JSONArray parse_item = (JSONArray) parse_items.get("item");
+		JSONObject parse_response = (JSONObject) obj.get("response");
+		JSONObject parse_body = (JSONObject) parse_response.get("body");
+		JSONObject parse_items = (JSONObject) parse_body.get("items");
+		JSONArray parse_item = (JSONArray) parse_items.get("item");
 
-        String category;
-        JSONObject weather;
-        Weather nowWeather = new Weather();
-        // 필요한 데이터만 가져오려고합니다.
-        for(int i = 0 ; i < parse_item.size(); i++){
-          weather = (JSONObject) parse_item.get(i);
-          String fcst_Value = weather.get("obsrValue").toString(); //실수로된 값과 정수로된 값이 둘다 있어서 실수로 통일했습니다.
-          category = (String)weather.get("category");
-          if (category.contains("T1H")){
-        	nowWeather.setT1h(fcst_Value);
-          } else if (category.contains("PTY")){
-        	nowWeather.setPty(fcst_Value);
-          } 
-          else if (category.contains("SKY")){
-          	nowWeather.setSky(fcst_Value);
-            }
-        }
-        System.out.println(nowWeather);
+		String category;
+		JSONObject weather;
+		Weather nowWeather = new Weather();
+
+		for(int i = 0 ; i < parse_item.size(); i++){
+			weather = (JSONObject) parse_item.get(i);
+			String fcst_Value = weather.get(serviceValue).toString();
+			category = (String)weather.get("category");
+
+			if (serviceValue.equals("obsrValue")) {
+				if (category.contains("T1H")){
+					nowWeather.setT1h(fcst_Value);
+				} else if (category.contains("PTY")){
+					nowWeather.setPty(fcst_Value);
+				} else if (category.contains("SKY")){
+					nowWeather.setSky(fcst_Value);
+				}
+			} else if (serviceValue.equals("fcstValue")) {
+				if (category.contains("TMN")){
+					todayTmn=fcst_Value;
+				} else if (category.contains("TMX")){
+					todayTmx=fcst_Value;
+				}
+			}
+		}
+		System.out.println(nowWeather);
+		System.out.println(todayTmn+":"+todayTmx);
 		return nowWeather;
 	}
 
-	public String getMinMaxWeather(){
-		// 오늘 최저, 최고 기온 조회
-		// 02시에 최저기온조회 후 tmn값변경
-		// 02, 05, 08, 11시에 최고기온 조회해서있으면 tmx값 변경
-		return null;
-	}
-
-	//@Scheduled(cron="* /45 * * * *")매시간 45분
-	//@Scheduled(cron="*/30 * * * * *")30초마다
-	public void setWeatherInformation() throws ParseException, IOException{
-		System.out.println("hi~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		Weather weatherInformation = new Weather();
-		weatherInformation = getNowWeather();
-		weatherInformation.setTmn(todayTmn);
-		weatherInformation.setTmx(todayTmx);
-		weatherService.createWeatherInformation(weatherInformation);
-	}
 }
