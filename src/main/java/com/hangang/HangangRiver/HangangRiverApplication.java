f43b5c7d80a77916b1b836d917c7c273df444df9
Unknown <kimts2002kr@naver.com>
2018-09-12 21:31:23 +0900

    #98 debug level 추가
    * level 추가
    * 서버 시작시 debug 로깅

--- src/main/java/com/hangang/HangangRiver/HangangRiverApplication.java
+++ src/main/java/com/hangang/HangangRiver/HangangRiverApplication.java
@@ -1,5 +1,8 @@
 package com.hangang.HangangRiver;
 
+import com.hangang.HangangRiver.common.web.ExceptionController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,7 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class HangangRiverApplication {
+	private static final Logger logger = LogManager.getLogger(ExceptionController.class);
+
 	public static void main(String[] args) {
+		logger.debug("Start");
 		SpringApplication.run(HangangRiverApplication.class, args);
 	}
 }
